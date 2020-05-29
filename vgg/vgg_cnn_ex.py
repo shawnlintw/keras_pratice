2c77239f2be094e2872d4470a859a4e0c130e2b4
shawnlin <edwin2619@gmail.com>
2020-05-29 18:56:12 +0800

    Changes to be committed: 	new file:   ../03/three_vgg_cnn.py__plot.png 	new file:   vgg_block.png 	new file:   vgg_cnn_ex.py

---
+++ vgg/vgg_cnn_ex.py
@@ -0,0 +1,22 @@
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D
+from keras.utils import plot_model
+
+def vgg_block(layer_in, n_filters, n_conv):
+	# add convolutional layers
+	for _ in range(n_conv):
+		layer_in = Conv2D(n_filters, (3,3), padding='same', activation='relu')(layer_in)
+	# add max pooling layer
+	layer_n = MaxPooling2D((2,2), strides=(2,2))(layer_in)
+	return layer_in
+
+# define model input
+visible= Input(shape=(256, 256,3))
+# add vgg module
+layer = vgg_block(visible, 64, 2)
+# create model
+model = Model(inputs=visible, outputs=layer)
+# summarize model
+model.summary()
+# plot model architecture
+plot_model(model, show_shapes=True, to_file='vgg_block.png')
