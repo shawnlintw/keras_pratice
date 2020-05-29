b52d6928b07a41b157e493235aca6b0a8b0e3659
shawnlin <edwin2619@gmail.com>
2020-05-29 19:10:59 +0800

    Changes to be committed: 	new file:   multi_vgg_block.py 	new file:   multiple_vgg_blocks.png 	new file:   readme 	modified:   vgg_block.png 	modified:   vgg_cnn_ex.py

--- vgg/multi_vgg_block.py
+++ vgg/multi_vgg_block.py
@@ -2,6 +2,7 @@ from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D
 from keras.utils import plot_model
 
+# function for creating a vgg block
 def vgg_block(layer_in, n_filters, n_conv):
 	# add convolutional layers
 	for _ in range(n_conv):
@@ -10,13 +11,17 @@ def vgg_block(layer_in, n_filters, n_conv):
 	layer_in = MaxPooling2D((2,2), strides=(2,2))(layer_in)
 	return layer_in
 
-# define model input
-visible= Input(shape=(256, 256,3))
+# define model input 
+visible = Input(shape=(256,256,3))
 # add vgg module
-layer = vgg_block(visible, 64, 2)
+layer = vgg_block(visible,64,2)
+# add vgg module
+layer = vgg_block(layer,128,2)
+# add vgg module
+layer = vgg_block(layer,256,4)
 # create model
 model = Model(inputs=visible, outputs=layer)
 # summarize model
 model.summary()
 # plot model architecture
-plot_model(model, show_shapes=True, to_file='vgg_block.png')
+plot_model(model, show_shapes=True, to_file="multiple_vgg_blocks.png")
